5e6397ed3780b35898401215b9147cc18321b32c
Sorokin Sergey <novak4803@gmail.com>
2018-02-19 18:12:36 +0300

    first veiw

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    My first App
+                    Laravel
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/myview', function () {
+    return view('myview');
+});
