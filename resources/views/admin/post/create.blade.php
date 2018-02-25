9df953b550d4e1426ffc98cd7a8114314100d5b6
Sorokin Sergey <novak4803@gmail.com>
2018-02-25 14:14:24 +0300

    test

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -1,13 +1,18 @@
 
-
 {!! Form::open(['url' => 'foo/bar']) !!}
 
+{{Form::label('username', 'User name')}}
+{{Form::text('username')}}</br></br>
+
 {{Form::label('email', 'E-Mail Address')}}
-{{Form::text('email', 'example@gmail.com')}}
+{{Form::text('email', 'example@gmail.com')}}</br></br>
 
+{{Form::label('password', 'password')}}
+{{Form::password('password', ['class' => 'awesome'])}}</br></br>
 
+{{Form::label('confirmPassword', 'Confirm password')}}
+{{Form::password('confirmPassword', ['class' => 'awesome'])}}</br></br>
 
-
-{{Form::submit('Click Me!')}}
+{{Form::submit('Зарегистрироваться')}}
 
 {!! Form::close() !!}
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,3 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
